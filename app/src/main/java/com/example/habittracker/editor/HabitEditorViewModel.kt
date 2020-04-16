ec1a8105fc1ff38c1d1664a1e52f3022aa337f48
Nikita Chernobrisov <nikistos@tutanota.com>
2020-04-16 16:24:29 +0500

    coroutine

--- app/src/main/java/com/example/habittracker/editor/HabitEditorViewModel.kt
+++ app/src/main/java/com/example/habittracker/editor/HabitEditorViewModel.kt
@@ -1,19 +1,31 @@
 package com.example.habittracker.editor
 
 import androidx.lifecycle.ViewModel
+import com.example.habittracker.database.AppDatabase
 import com.example.habittracker.database.HabitDao
 import com.example.habittracker.database.HabitEntity
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class HabitEditorViewModel(val database: HabitDao, val habitIndex: Int? = null) : ViewModel() {
+class HabitEditorViewModel(val database: HabitDao, val habitIndex: Int? = null) : ViewModel(),
+    CoroutineScope {
 
     var habit = if (habitIndex == null) HabitEntity() else database.getHabitById(habitIndex)
     val isNewHabit = (habitIndex == null)
 
-    fun updateHabit() {
-        database.update(habit)
+    private var viewModelJob = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + viewModelJob
+
+    fun updateHabit() = launch {
+        withContext(Dispatchers.IO) {
+            database.update(habit)
+        }
     }
 
-    fun addHabit() {
-        database.insert(habit)
+    fun addHabit() = launch {
+        withContext(Dispatchers.IO) {
+            database.insert(habit)
+        }
     }
 }
--- app/src/main/java/com/example/habittracker/editor/HabitEditorViewModelFactory.kt
+++ app/src/main/java/com/example/habittracker/editor/HabitEditorViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.example.habittracker.editor
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.habittracker.database.HabitDao
-import com.example.habittracker.habit_list_screen.HabitListViewModel
 
 class HabitEditorViewModelFactory(private val database: HabitDao, private val habitIndex: Int? = null):
     ViewModelProvider.NewInstanceFactory() {
